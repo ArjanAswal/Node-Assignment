b6e1a8f8676cd3cd753d320cb85a1ac28684fed0
Arjan Aswal <arjanaswal09@gmail.com>
2022-05-06 22:29:56 +0530

    fix a bug related to copying files

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Stop all servers and start the server
 cd /home/ubuntu
-cp env/.env my-app1
-cd /my-app1
+sudo cp env/.env my-app1
+cd my-app1
 
+# Stop all servers and start the server
 npm run stop
 npm run build
 npm run pm2
